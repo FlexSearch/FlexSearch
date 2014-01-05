0ca309763bd4fd13a7d1694903b6279fc85b20c3
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-01-05 23:18:58 +0000

    Added fsharp based test project
    Signed-off-by: Seemant Rajvanshi <seemant.rajvanshi@gmail.com>

---
+++ src/FlexSearch.Tests/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open FsUnit
+open Fuchu
+open System
+
+let simpleTest = 
+    testList "A simple test" [
+        for c in [1; 2; 3] ->
+            testCase ("Test " + c.ToString()) <| fun _ -> 
+                if c = 3 then
+                    failwith "c = 3 not valid"
+                c |> should equal c
+    ]
+
+
+[<EntryPoint>]
+let main argv = 
+    let result = run simpleTest
+    Console.WriteLine(result)
+    Console.ReadKey() |> ignore
+    result
+    
