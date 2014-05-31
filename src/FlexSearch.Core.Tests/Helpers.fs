0f15982af8574f605f6db4e2eb83d0b59fd0e445
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-05-31 11:32:22 +0100

    Added ExpectErrorCode test helper

--- src/FlexSearch.Core.Tests/Helpers.fs
+++ src/FlexSearch.Core.Tests/Helpers.fs
@@ -1,5 +1,4 @@
 ﻿namespace FlexSearch.Core.Tests
-
 open Xunit
 open FlexSearch.Api
 open FlexSearch.Api.Message
@@ -10,6 +9,7 @@ open Ploeh.AutoFixture
 open Ploeh.AutoFixture.AutoNSubstitute
 open Ploeh.AutoFixture.DataAnnotations
 open Ploeh.AutoFixture.Xunit
+open System
 open Xunit.Extensions
 
 [<AutoOpen>]
@@ -22,16 +22,26 @@ module Helpers =
         | Choice2Of2(error) -> 
             if expectedChoice1 then Assert.Equal(1, 2)
             else Assert.Equal(1, 1)
-    
-    let ExpectErrorCode (choice: Choice<'T, OperationMessage>) (operationMessage: OperationMessage) = 
+
+    let ExpectSuccess (choice : Choice<'T, OperationMessage>)  = 
         match choice with
         | Choice1Of2(success) -> 
-            Assert.True(1 = 2, "Expecting error but received success")
+            Assert.True(true)    
         | Choice2Of2(error) -> 
-            Assert.Equal(operationMessage.ErrorCode, error.ErrorCode)
-            
+            Assert.True(false, "Expected the result to be success but received failure: " + error.DeveloperMessage)
+                
+    let ExpectErrorCode (choice : Choice<'T, OperationMessage>) (operationMessage : OperationMessage) = 
+        match choice with
+        | Choice1Of2(success) -> Assert.True(1 = 2, "Expecting error but received success")
+        | Choice2Of2(error) -> Assert.Equal(operationMessage.ErrorCode, error.ErrorCode)
+    
     type DomainCustomization() = 
         inherit CompositeCustomization(new AutoNSubstituteCustomization(), new SupportMutableValueTypesCustomization())
     
     type AutoMockDataAttribute() = 
         inherit AutoDataAttribute((new Fixture()).Customize(new DomainCustomization()))
+    
+    [<AttributeUsage(AttributeTargets.Method, AllowMultiple = true)>]
+    type InlineAutoMockDataAttribute([<ParamArray>] values: Object[]) = 
+        inherit CompositeDataAttribute([| new InlineDataAttribute(values) :> DataAttribute
+                                          new AutoMockDataAttribute() :> DataAttribute |])
