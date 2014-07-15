1203e5e98baed9f9c31b89cc167f338162e3667b
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-07-15 21:45:47 +0100

    Removed top level module to make more consistent with .net

--- src/FlexSearch.Core/Utilities/PersitanceStore.fs
+++ src/FlexSearch.Core/Utilities/PersitanceStore.fs
@@ -20,124 +20,120 @@ open System.Collections.Generic
 open System.Net
 open System.Xml
 open System.Xml.Linq
+open System.Data.SQLite
+open System.IO
+open System.Linq
 
-[<AutoOpen>]
-module Store = 
-    open System.Data.SQLite
-    open System.IO
-    open System.Linq
+/// A reusable key value persistence store build on top of sql-lite
+[<Sealed>]
+type SqlLitePersistanceStore(?isMemory0 : bool) = 
+    let path = Path.Combine(Constants.ConfFolder, "Conf.db")
+    let isMemory = defaultArg isMemory0 false
+    let sqlCreateTable = """
+        CREATE TABLE [keyvalue] (
+        [id] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+        [key] TEXT  NOT NULL,
+        [value] TEXT  NOT NULL,
+        [type] TEXT  NOT NULL,
+        [timestamp] TIMESTAMP  NOT NULL
+        );
+
+        CREATE INDEX [keyvalue_idx] ON [keyvalue](
+        [key]  DESC,
+        [type]  DESC
+        );
+        """
+    let mutable db = None
     
-    /// A reusable key value persistence store build on top of sql-lite
-    [<Sealed>]
-    type PersistanceStore(?isMemory0 : bool) = 
-        let path = Path.Combine(Constants.ConfFolder, "Conf.db")
-        let isMemory = defaultArg isMemory0 false
-
-        let sqlCreateTable = """
-            CREATE TABLE [keyvalue] (
-            [id] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
-            [key] TEXT  NOT NULL,
-            [value] TEXT  NOT NULL,
-            [type] TEXT  NOT NULL,
-            [timestamp] TIMESTAMP  NOT NULL
-            );
-
-            CREATE INDEX [keyvalue_idx] ON [keyvalue](
-            [key]  DESC,
-            [type]  DESC
-            );
-            """
-        let mutable db = None
+    do 
+        let connectionString = 
+            if isMemory then "Data Source=:memory:;Version=3;New=True;UseUTF16Encoding=True;"
+            else sprintf "Data Source=%s;Version=3;UseUTF16Encoding=True;" path
         
-        do            
-            let connectionString = 
-                if isMemory then "Data Source=:memory:;Version=3;New=True;UseUTF16Encoding=True;"
-                else sprintf "Data Source=%s;Version=3;UseUTF16Encoding=True;" path
-            
-            let dbConnection = 
-                if isMemory then 
-                    let dbConnection = new SQLiteConnection(connectionString)
-                    dbConnection.Open()
-                    let command = new SQLiteCommand(sqlCreateTable, dbConnection)
-                    command.ExecuteNonQuery() |> ignore
-                    dbConnection
-                elif File.Exists(path) then 
-                    let dbConnection = new SQLiteConnection(connectionString)
-                    dbConnection.Open()
-                    dbConnection
-                else 
-                    SQLiteConnection.CreateFile(path)
-                    let dbConnection = new SQLiteConnection(connectionString)
-                    dbConnection.Open()
-                    let command = new SQLiteCommand(sqlCreateTable, dbConnection)
-                    command.ExecuteNonQuery() |> ignore
-                    dbConnection
-            
-            db <- Some(dbConnection)
+        let dbConnection = 
+            if isMemory then 
+                let dbConnection = new SQLiteConnection(connectionString)
+                dbConnection.Open()
+                let command = new SQLiteCommand(sqlCreateTable, dbConnection)
+                command.ExecuteNonQuery() |> ignore
+                dbConnection
+            elif File.Exists(path) then 
+                let dbConnection = new SQLiteConnection(connectionString)
+                dbConnection.Open()
+                dbConnection
+            else 
+                SQLiteConnection.CreateFile(path)
+                let dbConnection = new SQLiteConnection(connectionString)
+                dbConnection.Open()
+                let command = new SQLiteCommand(sqlCreateTable, dbConnection)
+                command.ExecuteNonQuery() |> ignore
+                dbConnection
         
-        member private this.Get<'T when 'T : equality> (key) = 
+        db <- Some(dbConnection)
+    
+    member private this.Get<'T when 'T : equality>(key) = 
+        let instanceType = typeof<'T>.FullName
+        if key = "" then Choice2Of2(MessageConstants.KEY_NOT_FOUND)
+        else 
+            let sql = sprintf "SELECT * FROM keyvalue WHERE type = '%s' AND key = '%s' LIMIT 1" instanceType key
+            let command = new SQLiteCommand(sql, db.Value)
+            let reader = command.ExecuteReader()
+            let mutable result : 'T = Unchecked.defaultof<'T>
+            while reader.Read() do
+                result <- Newtonsoft.Json.JsonConvert.DeserializeObject<'T>(reader.GetString(2))
+            if result <> Unchecked.defaultof<_> then Choice1Of2(result)
+            else Choice2Of2(MessageConstants.KEY_NOT_FOUND)
+    
+    interface IPersistanceStore with
+        member this.Get<'T when 'T : equality>(key : string) = this.Get<'T>(key)
+        
+        member this.GetAll<'T>() = 
+            let sql = sprintf "SELECT * FROM keyvalue WHERE type= '%s'" typeof<'T>.FullName
+            let command = new SQLiteCommand(sql, db.Value)
+            let reader = command.ExecuteReader()
+            let mutable result : List<'T> = new List<'T>()
+            while reader.Read() do
+                result.Add(Newtonsoft.Json.JsonConvert.DeserializeObject<'T>(reader.GetString(2)))
+            result :> IEnumerable<'T>
+        
+        member this.Delete<'T>(key) = 
             let instanceType = typeof<'T>.FullName
             if key = "" then Choice2Of2(MessageConstants.KEY_NOT_FOUND)
             else 
-                let sql = sprintf "SELECT * FROM keyvalue WHERE type = '%s' AND key = '%s' LIMIT 1" instanceType key
+                let sql = sprintf "DELETE FROM keyvalue WHERE type='%s' AND key='%s'" instanceType key
                 let command = new SQLiteCommand(sql, db.Value)
-                let reader = command.ExecuteReader()
-                let mutable result : 'T = Unchecked.defaultof<'T>
-                while reader.Read() do
-                    result <- Newtonsoft.Json.JsonConvert.DeserializeObject<'T>(reader.GetString(2))
-                if result <> Unchecked.defaultof<_> then Choice1Of2(result)
-                else Choice2Of2(MessageConstants.KEY_NOT_FOUND)
+                command.ExecuteNonQuery() |> ignore
+                Choice1Of2()
         
-        interface IPersistanceStore with
-            member this.Get<'T when 'T : equality>(key : string) = this.Get<'T> (key)
-            
-            member this.GetAll<'T>() = 
-                let sql = sprintf "SELECT * FROM keyvalue WHERE type= '%s'" typeof<'T>.FullName
+        member this.Put<'T> (key) (instance : 'T) = 
+            let instanceType = typeof<'T>.FullName
+            if key = "" then Choice2Of2(MessageConstants.KEY_NOT_FOUND)
+            else 
+                let sql = sprintf "SELECT * FROM keyvalue WHERE type='%s' AND key='%s' LIMIT 1" instanceType key
                 let command = new SQLiteCommand(sql, db.Value)
                 let reader = command.ExecuteReader()
-                let mutable result : List<'T> = new List<'T>()
+                let mutable result = false
                 while reader.Read() do
-                    result.Add(Newtonsoft.Json.JsonConvert.DeserializeObject<'T>(reader.GetString(2)))
-                result :> IEnumerable<'T>
-            
-            member this.Delete<'T>(key) = 
-                let instanceType = typeof<'T>.FullName
-                if key = "" then Choice2Of2(MessageConstants.KEY_NOT_FOUND)
-                else 
-                    let sql = sprintf "DELETE FROM keyvalue WHERE type='%s' AND key='%s'" instanceType key
+                    result <- true
+                let value = Newtonsoft.Json.JsonConvert.SerializeObject(instance)
+                if result then 
+                    // Exists so lets update it 
+                    let sql = 
+                        "UPDATE keyvalue SET type = @type, key = @key, value = @value, timestamp = @timestamp WHERE type = @type AND key = @key"
                     let command = new SQLiteCommand(sql, db.Value)
+                    command.Parameters.Add("@type", Data.DbType.String).Value <- instanceType
+                    command.Parameters.Add("@key", Data.DbType.String).Value <- key
+                    command.Parameters.Add("@value", Data.DbType.String).Value <- value
+                    command.Parameters.Add("@timestamp", Data.DbType.String).Value <- DateTime.Now.ToString()
                     command.ExecuteNonQuery() |> ignore
-                    Choice1Of2()
-            
-            member this.Put<'T> (key) (instance : 'T) = 
-                let instanceType = typeof<'T>.FullName
-                if key = "" then Choice2Of2(MessageConstants.KEY_NOT_FOUND)
                 else 
-                    let sql = sprintf "SELECT * FROM keyvalue WHERE type='%s' AND key='%s' LIMIT 1" instanceType key
+                    // Does not exist so create it
+                    let sql = 
+                        "INSERT INTO keyvalue (type, key, value, timestamp) VALUES (@type, @key, @value, @timestamp)"
                     let command = new SQLiteCommand(sql, db.Value)
-                    let reader = command.ExecuteReader()
-                    let mutable result = false
-                    while reader.Read() do
-                        result <- true
-                    let value = Newtonsoft.Json.JsonConvert.SerializeObject(instance)
-                    if result then 
-                        // Exists so lets update it 
-                        let sql = 
-                            "UPDATE keyvalue SET type = @type, key = @key, value = @value, timestamp = @timestamp WHERE type = @type AND key = @key"
-                        let command = new SQLiteCommand(sql, db.Value)
-                        command.Parameters.Add("@type", Data.DbType.String).Value <- instanceType
-                        command.Parameters.Add("@key", Data.DbType.String).Value <- key
-                        command.Parameters.Add("@value", Data.DbType.String).Value <- value
-                        command.Parameters.Add("@timestamp", Data.DbType.String).Value <- DateTime.Now.ToString()
-                        command.ExecuteNonQuery() |> ignore
-                    else 
-                        // Does not exist so create it
-                        let sql = 
-                            "INSERT INTO keyvalue (type, key, value, timestamp) VALUES (@type, @key, @value, @timestamp)"
-                        let command = new SQLiteCommand(sql, db.Value)
-                        command.Parameters.Add("@type", Data.DbType.String).Value <- instanceType
-                        command.Parameters.Add("@key", Data.DbType.String).Value <- key
-                        command.Parameters.Add("@value", Data.DbType.String).Value <- value
-                        command.Parameters.Add("@timestamp", Data.DbType.String).Value <- DateTime.Now.ToString()
-                        command.ExecuteNonQuery() |> ignore
-                    Choice1Of2()
+                    command.Parameters.Add("@type", Data.DbType.String).Value <- instanceType
+                    command.Parameters.Add("@key", Data.DbType.String).Value <- key
+                    command.Parameters.Add("@value", Data.DbType.String).Value <- value
+                    command.Parameters.Add("@timestamp", Data.DbType.String).Value <- DateTime.Now.ToString()
+                    command.ExecuteNonQuery() |> ignore
+                Choice1Of2()
