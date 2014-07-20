0558b94d6bda2750916a6e826d53682346c30dee
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-07-20 18:39:19 +0100

    Removed higher order function to enable compatibility with C#

--- src/FlexSearch.Core.Tests/PersistanceStoreTests.fs
+++ src/FlexSearch.Core.Tests/PersistanceStoreTests.fs
@@ -14,32 +14,32 @@ module ``Persistance Store Tests`` =
     
     let GetMemoryStore() = new SqlLitePersistanceStore(true) :> IPersistanceStore
     
-    [<Theory>][<AutoMockData>]
-    let ``Adding a new TestClass should pass`` (sut : TestClass) = ExpectSuccess(GetMemoryStore().Put "test" sut)
+    [<Theory; AutoMockData>]
+    let ``Adding a new TestClass should pass`` (sut : TestClass) = ExpectSuccess(GetMemoryStore().Put("test", sut))
     
-    [<Theory>][<AutoMockData>]
+    [<Theory; AutoMockData>]
     let ``Newly added value can be retrieved`` (sut : TestClass, key : string) = 
         let memoryStore = GetMemoryStore()
-        ExpectSuccess(memoryStore.Put key sut)
+        ExpectSuccess(memoryStore.Put(key, sut))
         let test = memoryStore.Get<TestClass>(key)
         let result = GetSuccessChoice test
         Assert.Equal<string>(sut.Property1, result.Property1)
         Assert.Equal(sut.Property2, result.Property2)
     
-    [<Theory>][<AutoMockData>]
+    [<Theory; AutoMockData>]
     let ``Updating a value by key is possible`` (sut : TestClass, key : string) = 
         let memoryStore = GetMemoryStore()
-        ExpectSuccess(memoryStore.Put key sut)
+        ExpectSuccess(memoryStore.Put(key, sut))
         let test1 = memoryStore.Get<TestClass>(key)
         let result = GetSuccessChoice test1
         Assert.Equal<string>(sut.Property1, result.Property1)
         Assert.Equal(sut.Property2, result.Property2)
     
-    [<Theory>][<AutoMockData>]
+    [<Theory; AutoMockData>]
     let ``After adding another record of type TestClass, GetAll should return 2`` (sut : TestClass, key1 : string, 
                                                                                    key2 : string) = 
         let memoryStore = GetMemoryStore()
-        ExpectSuccess(memoryStore.Put key1 sut)
-        ExpectSuccess(memoryStore.Put key2 sut)
+        ExpectSuccess(memoryStore.Put(key1, sut))
+        ExpectSuccess(memoryStore.Put(key2, sut))
         let result = memoryStore.GetAll<TestClass>()
         Assert.Equal(2, result.Count())
