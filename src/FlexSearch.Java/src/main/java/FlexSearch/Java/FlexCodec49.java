c1f1f267ed1e636b014f2e3e89bf1633e4d3dad7
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-08-06 20:13:58 +0100

    Implemented FlexCodec in Java

--- src/FlexSearch.Java/src/main/java/FlexSearch/Java/FlexCodec49.java
+++ src/FlexSearch.Java/src/main/java/FlexSearch/Java/FlexCodec49.java
@@ -1,3 +1,13 @@
+// ----------------------------------------------------------------------------
+// (c) Seemant Rajvanshi, 2013
+//
+// This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+// copy of the license can be found in the License.txt file at the root of this distribution. 
+// By using this source code in any fashion, you are agreeing to be bound 
+// by the terms of the Apache License, Version 2.0.
+//
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------
 package FlexSearch.Java;
 
 import org.apache.lucene.codecs.*;
@@ -5,19 +15,20 @@ import org.apache.lucene.codecs.lucene49.Lucene49Codec;
 
 public final class FlexCodec49 extends FilterCodec {
 
-    private PostingsFormat postingsFormat;
+    private final PostingsFormat postingsFormat = new FlexPerFieldPostingsFormat();
+    private final DocValuesFormat docValuesFormat = new FlexPerFieldDocValuesFormat();
 
     public FlexCodec49() {
         super("FlexCodec49", new Lucene49Codec());
     }
 
-    public FlexCodec49(PostingsFormat postingsFormat, Codec delegate) {
-        super("FlexCodec49", delegate);
-        this.postingsFormat = postingsFormat;
-    }
-
     @Override
     public final PostingsFormat postingsFormat() {
         return postingsFormat;
+    }
+
+    @Override
+    public final DocValuesFormat docValuesFormat() {
+        return docValuesFormat;
     }
 }
