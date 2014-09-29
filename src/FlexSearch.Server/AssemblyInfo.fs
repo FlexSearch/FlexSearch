f7716d6161e26c87a360f688750189e434159e7e
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-09-29 13:54:12 +0100

    Converted Main server app fro C# to F#

--- src/FlexSearch.Server/AssemblyInfo.fs
+++ src/FlexSearch.Server/AssemblyInfo.fs
@@ -1,33 +1,15 @@
-module FlexSearch.Server.AssemblyInfo
-#nowarn "49" // uppercase argument names
-#nowarn "67" // this type test or downcast will always hold
-#nowarn "66" // tis upast is unnecessary - the types are identical
-#nowarn "58" // possible incorrect indentation..
-#nowarn "57" // do not use create_DelegateEvent
-#nowarn "51" // address-of operator can occur in the code
-open System
+namespace System
+
 open System.Reflection
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
-exception ReturnException183c26a427ae489c8fd92ec21a0c9a59 of obj
-exception ReturnNoneException183c26a427ae489c8fd92ec21a0c9a59
 
-[<assembly: ComVisible (false)>]
+[<AssemblyTitleAttribute("FlexSearch Server")>]
+[<AssemblyDescriptionAttribute("FlexSearch Server")>]
+[<AssemblyProductAttribute("FlexSearch")>]
+[<AssemblyCopyrightAttribute("(c) Seemant Rajvanshi, 2012 - 2014")>]
+[<AssemblyFileVersionAttribute("0.21.1.0")>]
+[<AssemblyVersionAttribute("0.21.1.0")>]
+do ()
 
-[<assembly: CLSCompliant (false)>]
-
-[<assembly: Guid ("12cb2f85-01bf-4608-bb3d-902fbf97af1a")>]
-
-[<assembly: AssemblyTitle ("FlexSearch Server")>]
-
-[<assembly: AssemblyDescription ("FlexSearch Server")>]
-
-[<assembly: AssemblyProduct ("FlexSearch Server")>]
-
-[<assembly: AssemblyVersion ("0.4.0.0")>]
-
-[<assembly: AssemblyFileVersion ("0.4.0.0")>]
-
-[<assembly: AssemblyDelaySign (false)>]
-
-()
+module internal AssemblyVersionInformation = 
+    [<Literal>]
+    let Version = "0.21.1.0"
