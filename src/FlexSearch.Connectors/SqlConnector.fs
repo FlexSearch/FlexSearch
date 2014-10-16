789dc49dd95461935c230ac0645989444880bfc0
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-10-16 18:13:39 +0100

    Use messageprocessor for bulk request queuing

--- src/FlexSearch.Connectors/SqlConnector.fs
+++ src/FlexSearch.Connectors/SqlConnector.fs
@@ -37,12 +37,35 @@ type SqlSetting() =
 [<Sealed>]
 [<Name("POST-/indices/:id/sql/:id")>]
 type SqlHandler(serverSettings : ServerSettings, queueService : IQueueService, jobService : IJobService, threadSafeWriter : IThreadSafeWriter, logger : ILogService) = 
-    inherit HttpHandlerBase<unit, unit>()
+    inherit HttpHandlerBase<unit, Guid>()
+    
+    // Generate a sample file for user
+    let sampleFile = 
+        let sampleData = new ConcurrentDictionary<string, SqlSetting>()
+        let sqlSettings = new SqlSetting()
+        sqlSettings.ConnectionString <- "connectionString"
+        sqlSettings.Queries.TryAdd
+            ("query1", new SqlQuery(SingleRequestQuery = "singleRequestQuery1", BulkRequestQuery = "bulkRequestQuery1")) 
+        |> ignore
+        sqlSettings.Queries.TryAdd
+            ("query2", new SqlQuery(SingleRequestQuery = "singleRequestQuery2", BulkRequestQuery = "bulkRequestQuery2")) 
+        |> ignore
+        sampleData.TryAdd("SqlSettings1", sqlSettings) |> ignore
+        sampleData.TryAdd("SqlSettings2", sqlSettings) |> ignore
+        threadSafeWriter.WriteFile(Path.Combine(serverSettings.ConfFolder, "SqlConnector", "sample.yml"), sampleData)
     
     let settings = 
         match threadSafeWriter.ReadFile<ConcurrentDictionary<string, SqlSetting>>
-                  (Path.Combine(serverSettings.ConfFolder, "sql.yml")) with
-        | Choice1Of2(s) -> s
+                  (Path.Combine(serverSettings.ConfFolder, "SqlConnector", "sql.yml")) with
+        | Choice1Of2(s) -> 
+            let result = new ConcurrentDictionary<string, SqlSetting>(StringComparer.OrdinalIgnoreCase)
+            for setting in s do
+                let queries = new ConcurrentDictionary<string, SqlQuery>(StringComparer.OrdinalIgnoreCase)
+                for sql in setting.Value.Queries do
+                    queries.TryAdd(sql.Key, sql.Value) |> ignore
+                setting.Value.Queries <- queries
+                result.TryAdd(setting.Key, setting.Value) |> ignore
+            result
         | Choice2Of2(e) -> failwithf "%A" e
     
     let ExecuteSql(connectionString, query, indexName, jobId, forceCreate) = 
@@ -55,12 +78,11 @@ type SqlHandler(serverSettings : ServerSettings, queueService : IQueueService, j
             use reader = command.ExecuteReader()
             if reader.HasRows = true then 
                 while reader.Read() do
-                    let document = new Dictionary<string, string>()
-                    document.Add(Constants.IdField, reader.[0].ToString())
-                    for i = 1 to reader.FieldCount do
-                        document.Add(reader.GetName(i), reader.GetValue(i).ToString())
-                    if forceCreate then queueService.AddDocumentQueue(indexName, (reader.[0].ToString()), document)
-                    else queueService.AddOrUpdateDocumentQueue(indexName, (reader.[0].ToString()), document)
+                    let document = new FlexDocument(IndexName = indexName, Id = reader.[0].ToString())
+                    for i = 1 to reader.FieldCount - 1 do
+                        document.Fields.Add(reader.GetName(i), reader.GetValue(i).ToString())
+                    if forceCreate then queueService.AddDocumentQueue(document)
+                    else queueService.AddOrUpdateDocumentQueue(document)
                     rows <- rows + 1
                     if String.IsNullOrWhiteSpace(jobId) <> true && rows % 5000 = 0 then 
                         let job = 
@@ -90,6 +112,16 @@ type SqlHandler(serverSettings : ServerSettings, queueService : IQueueService, j
         | true, query -> Choice1Of2(query)
         | _ -> Choice2Of2("QUERY_NOT_FOUND:Query does not exist." |> GenerateOperationMessage)
     
+    let bulkRequestProcessor = 
+        MailboxProcessor.Start(fun inbox -> 
+            let rec loop = 
+                async { 
+                    let! (connectionString, query, indexName, jobId, forceCreate) = inbox.Receive()
+                    ExecuteSql(connectionString, query, indexName, jobId, forceCreate)
+                    return! loop
+                }
+            loop)
+    
     let ProcessRequest(index : string, connectionName : string, context : IOwinContext) = 
         maybe { 
             let queryName = GetValueFromQueryString "query" "default" context
@@ -100,13 +132,16 @@ type SqlHandler(serverSettings : ServerSettings, queueService : IQueueService, j
                 | null -> 
                     // Bulk request
                     let! query = GetQuery(queryName, c)
-                    ExecuteSql
-                        (c.ConnectionString, query.BulkRequestQuery, index, Guid.NewGuid().ToString(), forceCreate)
+                    let jobId = Guid.NewGuid()
+                    bulkRequestProcessor.Post
+                        (c.ConnectionString, query.BulkRequestQuery, index, jobId.ToString(), forceCreate)
+                    return! Choice1Of2(jobId)
                 | id -> 
                     // Single index request
                     let! query = GetQuery(queryName, c)
                     let singleRequestQuery = query.SingleRequestQuery.Replace("{id}", id)
                     ExecuteSql(c.ConnectionString, singleRequestQuery, index, "", forceCreate)
+                    return! Choice1Of2(Guid.Empty)
             | _ -> return! Choice2Of2("CONNECTION_NOT_FOUND:Connection does not exist." |> GenerateOperationMessage)
         }
     
