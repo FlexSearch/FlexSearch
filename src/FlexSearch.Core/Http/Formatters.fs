fd7b951cd98d15757ea8ddcac44734fd2594647b
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2014-11-05 15:59:14 +0000

    Feat(core) : Implement Jsonp support

--- src/FlexSearch.Core/Http/Formatters.fs
+++ src/FlexSearch.Core/Http/Formatters.fs
@@ -10,25 +10,14 @@
 // ----------------------------------------------------------------------------
 namespace FlexSearch.Core
 
-open FlexSearch
-open FlexSearch.Api
-open FlexSearch.Api.Messages
-open FlexSearch.Utility
 open Microsoft.Owin
 open Newtonsoft.Json
 open Newtonsoft.Json.Converters
-open ProtoBuf
-open System
-open System.Collections.Concurrent
 open System.IO
-open System.Linq
-open System.Net
-open System.Text
-open System.Threading
 
 [<Sealed>]
 type JilJsonFormatter() = 
-    let options = new Jil.Options(false, false, true, Jil.DateTimeFormat.ISO8601, false)
+    let options = new Jil.Options(false, false, false, Jil.DateTimeFormat.ISO8601, false)
     interface IFormatter with
         member x.SerializeToString(body : obj) = failwith "Not implemented yet"
         
@@ -39,7 +28,10 @@ type JilJsonFormatter() =
         member x.Serialize(body : obj, stream : Stream) : unit = 
             use writer = new StreamWriter(stream)
             Jil.JSON.Serialize(body, writer, options)
-            writer.Flush()
+        
+        member x.Serialize(body : obj, context : IOwinContext) = 
+            use writer = new StreamWriter(context.Response.Body)
+            Jil.JSON.Serialize(body, writer, options)
         
         member x.SupportedHeaders() : string [] = 
             [| "application/json"; "text/json"; "application/json;charset=utf-8"; "application/json; charset=utf-8" |]
@@ -59,17 +51,29 @@ type NewtonsoftJsonFormatter() =
             use writer = new StreamWriter(stream)
             let body = JsonConvert.SerializeObject(body, options)
             writer.Write(body)
-            writer.Flush()
+        
+        member x.Serialize(body : obj, context : IOwinContext) = 
+            use writer = new StreamWriter(context.Response.Body)
+            let body = JsonConvert.SerializeObject(body, options)
+            match context.Request.Query.Get("callback") with
+            | null -> writer.Write(body)
+            | value -> 
+                context.Response.ContentType <- "application/javascript"
+                writer.Write(value + "(")
+                writer.Write(body)
+                writer.Write(")")
         
         member x.SupportedHeaders() : string [] = 
             [| "application/json"; "text/json"; "application/json;charset=utf-8"; "application/json; charset=utf-8" |]
 
 [<Sealed>]
 type ProtoBufferFormatter() = 
+    let serialize (body : obj, stream : Stream) = ProtoBuf.Serializer.Serialize(stream, body)
     interface IFormatter with
         member x.SerializeToString(body : obj) = failwith "Not implemented yet"
         member x.DeSerialize<'T>(stream : Stream) = ProtoBuf.Serializer.Deserialize<'T>(stream)
-        member x.Serialize(body : obj, stream : Stream) : unit = ProtoBuf.Serializer.Serialize(stream, body)
+        member x.Serialize(body : obj, stream : Stream) : unit = serialize (body, stream)
+        member x.Serialize(body : obj, context : IOwinContext) : unit = serialize (body, context.Response.Body)
         member x.SupportedHeaders() : string [] = [| "application/x-protobuf"; "application/octet-stream" |]
 
 [<Sealed>]
@@ -77,6 +81,11 @@ type YamlFormatter() =
     let options = YamlDotNet.Serialization.SerializationOptions.EmitDefaults
     let serializer = new YamlDotNet.Serialization.Serializer(options)
     let deserializer = new YamlDotNet.Serialization.Deserializer(ignoreUnmatched = true)
+    
+    let serialize (body : obj, stream : Stream) = 
+        use TextWriter = new StreamWriter(stream)
+        serializer.Serialize(TextWriter, body)
+    
     interface IFormatter with
         
         member x.SerializeToString(body : obj) = 
@@ -88,8 +97,6 @@ type YamlFormatter() =
             use textReader = new StreamReader(stream)
             deserializer.Deserialize<'T>(textReader)
         
-        member x.Serialize(body : obj, stream : Stream) : unit = 
-            use TextWriter = new StreamWriter(stream)
-            serializer.Serialize(TextWriter, body)
-        
+        member x.Serialize(body : obj, stream : Stream) : unit = serialize (body, stream)
+        member x.Serialize(body : obj, context : IOwinContext) : unit = serialize (body, context.Response.Body)
         member x.SupportedHeaders() : string [] = [| "application/yaml" |]
