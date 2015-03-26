7a5d28bc14a008b2739f1a5e18c7d3bdb4da9fb5
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-03-26 17:01:12 +0000

    Tests : Added tests for validators in a new project
    Also add some method comments

---
+++ src/FlexSearch.Tests/Helpers.fs
@@ -0,0 +1,27 @@
+﻿module Helpers
+
+open Fixie
+open System
+open System.Linq
+open System.Reflection
+
+// ----------------------------------------------------------------------------
+// Convention Section for Fixie
+// ----------------------------------------------------------------------------
+/// Custom attribute to create parameterised test
+[<AttributeUsage(AttributeTargets.Method, AllowMultiple = true)>]
+type InlineDataAttribute([<System.ParamArrayAttribute>] parameters : obj []) = 
+    inherit Attribute()
+    member val Parameters = parameters
+
+type FromInputAttributes() = 
+    interface ParameterSource with
+        member this.GetParameters(meth : MethodInfo) = 
+            meth.GetCustomAttributes<InlineDataAttribute>(true).Select(fun input -> input.Parameters)
+
+type SingleInstancePerClassConvention() as self = 
+    inherit Convention()
+    do 
+        self.Classes.NameEndsWith([| "Tests"; "Test" |]) |> ignore
+        self.ClassExecution.CreateInstancePerClass() |> ignore
+        self.Parameters.Add<FromInputAttributes>() |> ignore
---
+++ src/FlexSearch.Tests/InfrastructureTests.fs
@@ -0,0 +1,62 @@
+﻿module InfrastructureTests
+
+open FlexSearch.Core
+open Helpers
+open Swensen.Unquote
+
+type ValidatorTests() = 
+    let grpName = "grpName"
+    let fldName = "fldName"
+    
+    member __.HasDuplicateSuccessTest() = 
+        let sut = [| "a"; "a"; "b" |]
+        test <@ Validators.hasDuplicates grpName fldName sut = fail (DuplicateFieldValue(grpName, fldName)) @>
+    
+    member __.HasDuplicateFailureTest() = 
+        let sut = [| "a"; "c"; "b" |]
+        test <@ Validators.hasDuplicates grpName fldName sut = ok() @>
+    
+    member __.GreaterThanSuccessTest() = test <@ Validators.gt fldName 4 5 = ok() @>
+    member __.GreaterThanFailureTest() = test <@ Validators.gt fldName 5 4 = fail (GreaterThan(fldName, "5", "4")) @>
+    member __.GreaterThanEqualSuccessTest() = test <@ Validators.gte fldName 5 5 = ok() @>
+    member __.GreaterThanEqualFailureTest() = 
+        test <@ Validators.gte fldName 5 4 = fail (GreaterThanEqual(fldName, "5", "4")) @>
+    member __.LessThanSuccessTest() = test <@ Validators.lessThan fldName 5 4 = ok() @>
+    member __.LessThanFailureTest() = test <@ Validators.lessThan fldName 4 5 = fail (LessThan(fldName, "4", "5")) @>
+    member __.LessThanEqualSuccessTest() = test <@ Validators.lessThanEqual fldName 5 5 = ok() @>
+    member __.LessThanEqualFailureTest() = 
+        test <@ Validators.lessThanEqual fldName 4 5 = fail (LessThanEqual(fldName, "4", "5")) @>
+    member __.NotBlankSuccessTest() = test <@ Validators.notBlank fldName "test" = ok() @>
+    member __.NotBlankFailureTest() = test <@ Validators.notBlank fldName "" = fail (NotBlank(fldName)) @>
+    
+    [<InlineData("test")>]
+    [<InlineData("1234")>]
+    [<InlineData("_test")>]
+    member __.PropertyValidatorSuccessTests(sut : string) = 
+        test <@ Validators.propertyNameValidator fldName sut = ok() @>
+    
+    [<InlineData("TEST")>]
+    [<InlineData("Test")>]
+    [<InlineData(Constants.IdField)>]
+    [<InlineData(Constants.LastModifiedField)>]
+    [<InlineData("<test>")>]
+    member __.PropertyValidatorFailureTests(sut : string) = 
+        test <@ Validators.propertyNameValidator fldName sut = fail (InvalidPropertyName(fldName, sut)) @>
+
+type SeqValidatorTests() = 
+    
+    let implValid = 
+        { new IValidate with
+              member this.Validate() = ok() }
+    
+    let implInvaid = 
+        { new IValidate with
+              member this.Validate() = fail (InvalidPropertyName("test", "test")) }
+    
+    member __.``Test will only succeed when all elements are valid.``() = 
+        let sut = [| implValid; implValid; implValid |]
+        test <@ Validators.seqValidator sut = ok() @>
+    
+    member __.``Test will fail for even a single invalid item in the seq.``() = 
+        let sut = [| implValid; implValid; implInvaid |]
+        test <@ Validators.seqValidator sut = fail (InvalidPropertyName("test", "test")) @>
