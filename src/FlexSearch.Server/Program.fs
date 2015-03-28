a83b9c3e95db3b20213e59cf4c0b56eda931f5d0
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-03-28 14:12:19 +0000

    Build : Added FlexSearch.Server project back to the solution

--- src/FlexSearch.Server/Program.fs
+++ src/FlexSearch.Server/Program.fs
@@ -1,28 +1,28 @@
-﻿open FlexSearch.Core
+﻿open EventSourceProxy.NuGet
+open FlexSearch.Core
 open System
 open System.IO
 open Topshelf
-open FlexSearch.Logging
 
 [<EntryPoint>]
 let main argv = 
-    let logger = LogService.GetLogger(false)
-    // Capture all unhandled errors
-    AppDomain.CurrentDomain.UnhandledException.Subscribe
-        (fun x -> logger.TraceError(sprintf "Unhandled exception in application: %A" x.ExceptionObject)) 
-    |> ignore
-    // Load server settings
+    //    // Capture all unhandled errors
+    //    AppDomain.CurrentDomain.UnhandledException.Subscribe
+    //        (fun x -> logger.TraceError(sprintf "Unhandled exception in application: %A" x.ExceptionObject)) 
+    //    |> ignore
+    //    // Load server settings
     let settings = 
-        match ServerSettings.GetFromFile(Path.Combine(Constants.ConfFolder, "Config.yml"), new YamlFormatter() :> IFormatter) with
+        match ServerSettings.createFromFile 
+                  (Path.Combine(Constants.ConfFolder, "Config.yml"), new Formatters.YamlFormatter() :> IFormatter) with
         | Choice1Of2(s) -> s
         | Choice2Of2(e) -> 
-            logger.TraceError("Error parsing 'Config.yml' file.", e)
-            failwithf "%s" e.UserMessage
+            //            logger.TraceError("Error parsing 'Config.yml' file.", e)
+            failwithf "%s" (e.ToString())
     // Load all plug-in DLLs
     for file in Directory.EnumerateFiles(Constants.PluginFolder, "*.dll", SearchOption.TopDirectoryOnly) do
         try 
             System.Reflection.Assembly.LoadFile(file) |> ignore
-        with e -> logger.TraceError("Error loading plug-in library.", e)
+        with e -> () //logger.TraceError("Error loading plug-in library.", e)
     let TopShelfConfiguration() = 
         HostFactory.Run(fun conf -> 
             conf.RunAsLocalSystem() |> ignore
@@ -32,8 +32,8 @@ let main argv =
             conf.StartAutomatically() |> ignore
             conf.EnableServiceRecovery(fun rc -> rc.RestartService(1) |> ignore) |> ignore
             conf.Service<NodeService>(fun factory -> 
-                ServiceConfiguratorExtensions.ConstructUsing(factory, fun () -> new NodeService(settings, logger, false)) 
-                |> ignore
+                ServiceConfiguratorExtensions.ConstructUsing
+                    (factory, fun () -> new NodeService(settings, Log.logger, false)) |> ignore
                 ServiceConfiguratorExtensions.WhenStarted(factory, fun tc -> tc.Start()) |> ignore
                 ServiceConfiguratorExtensions.WhenStopped(factory, fun tc -> tc.Stop()) |> ignore)
             |> ignore)
