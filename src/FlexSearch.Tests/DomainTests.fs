a7ffb03b05d0dedc60b652524f126d5616255f25
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-03-28 14:15:10 +0000

    Tests : Added analyzer tests

--- src/FlexSearch.Tests/DomainTests.fs
+++ src/FlexSearch.Tests/DomainTests.fs
@@ -1,2 +1,23 @@
-﻿namespace DomainTests
+﻿module DomainTests
 
+open FlexSearch.Core
+open System.Collections.Generic
+open Swensen.Unquote
+
+module AnalyzerTests = 
+    open Analyzer
+    
+    type BuilderTests() = 
+        member __.``Should build successfully``() = 
+            let sut = 
+                { Tokenizer = { Tokenizer.Default with TokenizerName = "standard" }
+                  Filters = new List<TokenFilter>()
+                  AnalyzerName = "test" }
+            test <@ succeeded <| Analyzer.build sut = true @>
+        
+        member __.``Should not build successfully``() = 
+            let sut = 
+                { Tokenizer = { Tokenizer.Default with TokenizerName = "unknown" }
+                  Filters = new List<TokenFilter>()
+                  AnalyzerName = "test" }
+            test <@ failed <| Analyzer.build sut = true @>
