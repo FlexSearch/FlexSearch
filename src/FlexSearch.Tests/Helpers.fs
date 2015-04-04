f5c5e8951b25b102864373ba73bc9f5a0dab3ef4
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-04-04 22:13:50 +0100

    Tests : Implemented AutoFixture support in Fixie

--- src/FlexSearch.Tests/Helpers.fs
+++ src/FlexSearch.Tests/Helpers.fs
@@ -2,6 +2,9 @@
 module Helpers
 
 open Fixie
+open FlexSearch.Core
+open Ploeh.AutoFixture
+open Ploeh.AutoFixture.Kernel
 open System
 open System.Linq
 open System.Reflection
@@ -15,19 +18,29 @@ type InlineDataAttribute([<System.ParamArrayAttribute>] parameters : obj []) =
     inherit Attribute()
     member val Parameters = parameters
 
-type FromInputAttributes() = 
+type InputParameterSource() = 
     interface ParameterSource with
-        member this.GetParameters(meth : MethodInfo) = 
-            meth.GetCustomAttributes<InlineDataAttribute>(true).Select(fun input -> input.Parameters)
+        member __.GetParameters(methodInfo : MethodInfo) = 
+            // Check if the method contains inline data attribute. If not then use AutoFixture
+            // to generate input value
+            let customAttribute = methodInfo.GetCustomAttributes<InlineDataAttribute>(true)
+            if customAttribute.Any() then 
+                customAttribute.Select(fun input -> input.Parameters)
+            else
+                let fixture = new Ploeh.AutoFixture.Fixture()
+                let create (builder : ISpecimenBuilder, typ : Type) = (new SpecimenContext(builder)).Resolve(typ)
+                let parameterTypes = methodInfo.GetParameters().Select(fun x -> x.ParameterType)
+                let parameterValues = parameterTypes.Select(fun x -> create (fixture, x)).ToArray()
+                seq { yield parameterValues }
+            
 
 type SingleInstancePerClassConvention() as self = 
     inherit Convention()
     do 
         self.Classes.NameEndsWith([| "Tests"; "Test" |]) |> ignore
         self.ClassExecution.CreateInstancePerClass() |> ignore
-        self.Parameters.Add<FromInputAttributes>() |> ignore
-
+        self.Parameters.Add<InputParameterSource>() |> ignore
+        
 type SingleInstancePerClassConvention1() as self = 
     inherit Convention()
-    do 
-        self.Methods.Where(fun x -> x.IsStatic && x.IsPublic) |> ignore
+    do self.Methods.Where(fun x -> x.IsStatic && x.IsPublic) |> ignore
