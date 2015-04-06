0dc8d50451096b81850e939c2fc1fd0aea332647
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-04-06 10:49:58 +0100

    Refactor(Rest) : Added index based rest services back to the project

--- src/FlexSearch.Core/HttpService.fs
+++ src/FlexSearch.Core/HttpService.fs
@@ -17,3 +17,81 @@
 // ----------------------------------------------------------------------------
 namespace FlexSearch.Core
 
+///  Get all indices
+[<Name("GET-/indices")>]
+[<Sealed>]
+type GetAllIndexHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<NoBody, Index.Dto []>()
+    override __.Process(_, _) = SuccessResponse(indexService.GetAllIndex(), Ok)
+
+///  Get an index
+[<Name("GET-/indices/:id")>]
+[<Sealed>]
+type GetIndexByIdHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<NoBody, Index.Dto>(true)
+    override __.Process(request, _) = SomeResponse(indexService.GetIndex(request.ResId.Value), Ok, NotFound)
+
+/// Create an index
+[<Name("POST-/indices")>]
+[<Sealed>]
+type PostIndexByIdHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<Index.Dto, CreateResponse>()
+    override __.Process(request, body) = 
+        match indexService.AddIndex(body.Value) with
+        | Choice1Of2(response) -> SuccessResponse(response, Created)
+        | Choice2Of2(error) -> 
+            if error = IndexAlreadyExists(body.Value.IndexName) then FailureResponse(error, Conflict)
+            else FailureResponse(error, BadRequest)
+
+/// Delete an index
+[<Name("DELETE-/indices/:id")>]
+[<Sealed>]
+type DeleteIndexByIdHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<NoBody, unit>()
+    override __.Process(request, body) = SomeResponse(indexService.DeleteIndex(request.ResId.Value), Ok, BadRequest)
+
+/// Update an index
+[<Name("PUT-/indices/:id")>]
+[<Sealed>]
+type PutIndexByIdHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<Index.Dto, unit>()
+    override __.Process(request, body) = 
+        // Index name passed in URL takes precedence
+        body.Value.IndexName <- request.ResId.Value
+        SomeResponse(indexService.UpdateIndex(body.Value), Ok, BadRequest)
+
+type IndexStatusResponse() = 
+    member val Status = Unchecked.defaultof<IndexState> with get, set
+
+/// Get index status
+[<Name("GET-/indices/:id/status")>]
+[<Sealed>]
+type GetStatusHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<NoBody, IndexStatusResponse>()
+    override __.Process(request, body) = 
+        let response = 
+            match indexService.GetIndexStatus(request.ResId.Value) with
+            | Choice1Of2(state) -> Choice1Of2(new IndexStatusResponse(Status = state))
+            | Choice2Of2(error) -> Choice2Of2(error)
+        SomeResponse(response, Ok, BadRequest)
+
+/// Update index status
+[<Name("PUT-/indices/:id/status/:id")>]
+[<Sealed>]
+type PutStatusHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<NoBody, unit>()
+    override this.Process(request, body) = 
+        match request.SubResId.Value with
+        | InvariantEqual "online" -> SomeResponse(indexService.OpenIndex(request.ResId.Value), Ok, BadRequest)
+        | InvariantEqual "offline" -> SomeResponse(indexService.CloseIndex(request.ResId.Value), Ok, BadRequest)
+        | _ -> FailureResponse(HttpNotSupported, BadRequest)
+
+/// Check if an index exists
+[<Name("GET-/indices/:id/exists")>]
+[<Sealed>]
+type GetExistsHandler(indexService : IIndexService) = 
+    inherit HttpHandlerBase<NoBody, IndexExistsResponse>()
+    override this.Process(request, body) = 
+        match indexService.IndexExists(request.ResId.Value) with
+        | true -> SuccessResponse(new IndexExistsResponse(Exists = true), Ok)
+        | false -> FailureResponse(IndexNotFound(request.ResName), NotFound)
