1db863e2cb4fe9f6fb4610b95850d815d4cd3a75
vladnega <negacevschi.vladimir@gmail.com>
2015-04-21 16:05:54 +0100

    Feature(connector) : Add SQL connector

--- src/FlexSearch.Core/Connectors.fs
+++ src/FlexSearch.Core/Connectors.fs
@@ -2,9 +2,13 @@
 
 open System
 open System.IO
+open System.Data
+open System.Data.SqlClient
 open CsvHelper
 open CsvHelper.Configuration
 open Microsoft.Owin
+
+
 
 
 /// <summary>
@@ -133,5 +137,123 @@ type CsvHandler(queueService : IQueueService, jobService : IJobService) =
         >>= pathValidation
         >>= postBulkRequestMessage
 
+    override this.Process(request, body) = 
+        SomeResponse(processRequest request.ResName body.Value, Ok, BadRequest)
+
+
+
+/// <summary>
+/// Represents a request which can be sent to Sql connector to index SQL data
+/// </summary>
+[<Sealed>]
+type SqlIndexingRequest() = 
+    inherit DtoBase()
+    
+    /// <summary>
+    /// Name of the index
+    /// </summary>
+    member val IndexName = defString with get, set
+    
+    /// <summary>
+    /// The query to be used to fetch data from Sql server
+    /// </summary>
+    member val Query = defString with get, set
+    
+    /// <summary>
+    /// Connection string used to connect to the server
+    /// </summary>
+    member val ConnectionString = defString with get, set
+    
+    /// <summary>
+    /// Signifies if all updates to the index are create
+    /// </summary>
+    member val ForceCreate = true with get, set
+    
+    /// <summary>
+    /// Signifies if the connector should create a job for the task and return a jobId which can be used
+    /// to check the status of the job.
+    /// </summary>
+    member val CreateJob = false with get, set
+
+    override __.Validate() = ok ()
+
+[<Sealed>]
+[<Name("POST-/indices/:id/sql")>]
+type SqlHandler(queueService : IQueueService, jobService : IJobService) = 
+    inherit HttpHandlerBase<SqlIndexingRequest, string>()
+    
+    let ExecuteSql(request : SqlIndexingRequest, jobId) = 
+        let isNotBlank = isBlank >> not
+
+        if request.CreateJob then 
+            let job = new Job(JobId = jobId, Status = JobStatus.InProgress)
+            jobService.UpdateJob(job) |> ignore
+
+        try 
+            use connection = new SqlConnection(request.ConnectionString)
+            use command = new SqlCommand(request.Query, Connection = connection, CommandType = CommandType.Text)
+            command.CommandTimeout <- 300
+            connection.Open()
+            let mutable rows = 0
+            
+            use reader = command.ExecuteReader()
+
+            if reader.HasRows then 
+                while reader.Read() do
+                    let document = new Document.Dto(IndexName = request.IndexName, Id = reader.[0].ToString())
+                    for i = 1 to reader.FieldCount - 1 do
+                        document.Fields.Add(reader.GetName(i), reader.GetValue(i).ToString())
+                    if request.ForceCreate then queueService.AddDocumentQueue(document)
+                    else queueService.AddOrUpdateDocumentQueue(document)
+                    rows <- rows + 1
+                    if jobId |> isNotBlank && rows % 5000 = 0 then 
+                        let job = 
+                            new Job(JobId = jobId, Status = JobStatus.InProgress, Message = "", ProcessedItems = rows)
+                        jobService.UpdateJob(job) |> ignore
+
+                if jobId |> isNotBlank then 
+                    let job = 
+                        new Job(JobId = jobId, Status = JobStatus.Completed, Message = "Completed", 
+                                ProcessedItems = rows)
+                    jobService.UpdateJob(job) |> ignore
+                    Log.info <| sprintf "SQL connector: %A" job
+            else 
+                if jobId |> isNotBlank then 
+                    let job = 
+                        new Job(JobId = jobId, Status = JobStatus.CompletedWithErrors, Message = "No rows returned.", 
+                                ProcessedItems = rows)
+                    jobService.UpdateJob(job) |> ignore
+                Log.error <| sprintf "SQL connector error. No rows returned. Query:{%s}" request.Query
+        with e -> 
+            if jobId |> isNotBlank then 
+                let job = new Job(JobId = jobId, Status = JobStatus.CompletedWithErrors, Message = e.Message)
+                jobService.UpdateJob(job) |> ignore
+            Log.error <| sprintf "SQL connector error: %s" (e |> exceptionPrinter)
+    
+    let bulkRequestProcessor = 
+        MailboxProcessor.Start(fun inbox -> 
+            let rec loop() = 
+                async { 
+                    let! (body, jobId) = inbox.Receive()
+                    ExecuteSql(body, jobId)
+                    return! loop()
+                }
+            loop())
+    
+    let processRequest index (body : SqlIndexingRequest) = 
+        let createJob () = 
+            if body.CreateJob then 
+                let guid = Guid.NewGuid()
+                bulkRequestProcessor.Post(body, guid.ToString())
+                guid.ToString()
+            else 
+                ExecuteSql(body, "")
+                ""
+            |> Choice1Of2
+        body.IndexName <- index
+
+        body.Validate()
+        >>= createJob
+    
     override this.Process(request, body) = 
         SomeResponse(processRequest request.ResName body.Value, Ok, BadRequest)
