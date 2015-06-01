4b05e6d6d6ba26427eccfaf19dd90a3c66b665f5
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-06-01 14:22:40 +0100

    Fix(Http) : Don't overwrite searchprofile value when it is not passed as part of query string

--- src/FlexSearch.Core/HttpService.fs
+++ src/FlexSearch.Core/HttpService.fs
@@ -364,7 +364,7 @@ type GetSearchHandler(searchService : ISearchService) =
         query.Skip <- request.OwinContext |> intFromQueryString "skip" query.Skip
         query.OrderBy <- request.OwinContext |> stringFromQueryString "orderby" query.OrderBy
         query.ReturnFlatResult <- request.OwinContext |> boolFromQueryString "returnflatresult" query.ReturnFlatResult
-        query.SearchProfile <- request.OwinContext |> stringFromQueryString "searchprofile" ""
+        query.SearchProfile <- request.OwinContext |> stringFromQueryString "searchprofile" query.SearchProfile
         query.IndexName <- request.ResId.Value
         match searchService.Search(query) with
         | Choice1Of2(result) -> 
