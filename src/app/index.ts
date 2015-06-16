00804f6e81328998f15c2819e9303e10a3414132
vladnega <negacevschi.vladimir@gmail.com>
2015-06-16 14:22:34 +0100

    feat(portal): Use ui-router to do the roting instead of ngRouter

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,16 +1,15 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-/// <reference path="main/main.controller.ts" />
+/// <reference path="../app/partials/main.controller.ts" />
 /// <reference path="../app/components/navbar/navbar.controller.ts" />
-/// <reference path="../app/sessions/session.controller.ts" />
-/// <reference path="../app/sessions/sessions.controller.ts" />
+/// <reference path="../app/partials/session.controller.ts" />
+/// <reference path="../app/partials/sessions.controller.ts" />
 
 module flexportal {
   'use strict';
 
-  angular.module('flexportal', ['ngAnimate', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute', 'ngMaterial'])
+  angular.module('flexportal', ['ngAnimate', 'ngTouch', 'ngSanitize', 'restangular', 'ngMaterial', 'ui.router'])
     .controller('MainCtrl', MainCtrl)
-    .controller('NavbarCtrl', NavbarCtrl)
     .controller('SessionController', SessionController)
     .controller('SessionsController', SessionsController)
     .config(function($mdThemingProvider: ng.material.MDThemingProvider) {
@@ -18,23 +17,42 @@ module flexportal {
         .primaryPalette('green')
         .accentPalette('teal');
     })
-    .config(function($routeProvider: ng.route.IRouteProvider) {
-      $routeProvider
-        .when('/main', {
-          templateUrl: 'app/main/main.html',
-          controller: MainCtrl
+    // .config(function($routeProvider: ng.route.IRouteProvider) {
+    //   $routeProvider
+    //     .when('/main', {
+    //       templateUrl: 'app/main/main.html',
+    //       controller: MainCtrl
+    //     })
+    //     .when('/session/:id', {
+    //       templateUrl: 'app/sessions/session.html',
+    //       controller: SessionController
+    //     })
+    //     .when('/sessions', {
+    //       templateUrl: 'app/sessions/sessions.html',
+    //       controller: SessionsController
+    //     })
+    //     .otherwise({
+    //       redirectTo: '/main'
+    //     });
+    // })
+    .config(function($stateProvider: angular.ui.IStateProvider, $urlRouterProvider : angular.ui.IUrlRouterProvider){
+      $urlRouterProvider.otherwise("/main");
+      
+      $stateProvider
+        .state('main', {
+          url: "/main",
+          templateUrl: "app/partials/main.html",
+          controller: 'MainCtrl'
         })
-        .when('/session/:id', {
-          templateUrl: 'app/sessions/session.html',
-          controller: SessionController
+        .state('sessions', {
+          url: "/sessions",
+          templateUrl: "app/partials/sessions.html",
+          controller: 'SessionsController'
         })
-        .when('/sessions', {
-          templateUrl: 'app/sessions/sessions.html',
-          controller: SessionsController
-        })
-        .otherwise({
-          redirectTo: '/main'
+        .state('session', {
+          url: "/session/:id",
+          templateUrl: "app/partials/session.html",
+          controller: 'SessionController'
         });
-    })
-  ;
+    });
 }
