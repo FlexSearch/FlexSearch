ca287c975c20b81888afcb71ca3a996f7fe43c8e
vladnega <negacevschi.vladimir@gmail.com>
2015-06-19 10:25:15 +0100

    feat(portal): Navigate to a session when clicking on a row in Sessions table

--- src/app/views/sessions/sessions.controller.ts
+++ src/app/views/sessions/sessions.controller.ts
@@ -2,34 +2,33 @@
 
 module flexportal {
   'use strict';
-  
+
   class Session extends FlexSearch.DuplicateDetection.Session {
-    JobStartTimeString: string 
-    JobEndTimeString: string 
+    JobStartTimeString: string
+    JobEndTimeString: string
   }
-  
+
   interface ISessionsProperties extends ng.IScope {
     Sessions: Session[]
     Limit: number
     Page: number
     Total: number
+    goToSession(sessionId: string): void
   }
-  
+
   export class SessionsController {
     /* @ngInject */
-    constructor($scope: ISessionsProperties, $http: ng.IHttpService) {
+    constructor($scope: ISessionsProperties, $state: any, $http: ng.IHttpService) {
       $http.get("http://localhost:9800/indices/duplicates/search?c=*&q=type+=+'session'").then((response: any) => {
-        $scope.Sessions = [];
-        $scope.Limit = 1;
-        $scope.Page = 5;
-        $scope.Total = 5;
-        console.log(response.data);
-        
-        var toDateStr = function(dateStr: any){
+        $scope.goToSession = function(sessionId) {
+          $state.go('session', {id: sessionId});
+        };
+
+        var toDateStr = function(dateStr: any) {
           var date = new Date(dateStr);
           return date.toLocaleDateString() + ", " + date.toLocaleTimeString();
         }
-        
+
         var results = <FlexSearch.Core.SearchResults>response.data.Data;
         $scope.Sessions = results.Documents
           .map(d => <Session>JSON.parse(d.Fields["sessionproperties"]))
@@ -41,16 +40,6 @@ module flexportal {
         $scope.Limit = 10;
         $scope.Page = 1;
         $scope.Total = results.TotalAvailable;
-        
-        // for (var i = 0; i < response.data.Data.length; i++) {
-        //   var data = response.data.Data[i];
-        //   console.log(data);
-        //   var sessionProperties = JSON.parse(data.sessionproperties);
-        //   // var session = ;
-        //   // session.IndexName = sessionProperties.IndexName;
-        //   // session.ProfileName = sessionProperties.ProfileName;
-        //   // $scope.Sessions.push(session);
-        // }
       });
     }
   }
