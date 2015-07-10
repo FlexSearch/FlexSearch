e72f3c38192f68b40a4b8464863d52e2266caea0
vladnega <negacevschi.vladimir@gmail.com>
2015-07-10 16:30:45 +0100

    Use Invoke-Expression in PS script to wait for completion

--- srcjs/build.ps1
+++ srcjs/build.ps1
@@ -1,5 +1,5 @@
-npm install
-bower install
-tsd update --save
-gulp
-
+Invoke-Expression "npm install"
+Invoke-Expression "bower install"
+Invoke-Expression "tsd update --save"
+Invoke-Expression gulp
+echo "Finished portal build script"
