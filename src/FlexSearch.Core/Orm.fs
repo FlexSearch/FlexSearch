7ada05db9f3e9fd9a1fef756d790e325b0b00f81
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-08-12 17:05:53 +0100

    feat(core) : added an orm mapper to map poco to indexable documents

---
+++ src/FlexSearch.Core/Orm.fs
@@ -0,0 +1,55 @@
+﻿// ----------------------------------------------------------------------------
+//  Licensed to FlexSearch under one or more contributor license 
+//  agreements. See the NOTICE file distributed with this work 
+//  for additional information regarding copyright ownership. 
+//
+//  This source code is subject to terms and conditions of the 
+//  Apache License, Version 2.0. A copy of the license can be 
+//  found in the License.txt file at the root of this distribution. 
+//  You may also obtain a copy of the License at:
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  By using this source code in any fashion, you are agreeing
+//  to be bound by the terms of the Apache License, Version 2.0.
+//
+//  You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------
+namespace FlexSearch.Core
+
+/// The purpose of this module is to provide generic helper to map 
+/// internal types to FlexSearch documents so that it could be used
+/// to easily add/retrieve strongly typed objects from FlexSearch 
+/// indices. This module will be used for logging, duplicates, jobs 
+/// etc.
+module Orm =
+    open FastMember
+    open System.Collections.Concurrent
+    open System.Collections.Generic
+    open System
+
+    let private typeRepository = conDict<TypeAccessor>()
+    let private dateTimeType = typeof<DateTime>.FullName
+
+    let inline private generateFields<'T> (instance : 'T) (accessor : TypeAccessor) =
+        let doc = new Dictionary<string,string>()
+        for m in accessor.GetMembers() do
+            if m.Type.FullName = dateTimeType then
+                let date = accessor.[instance, m.Name] :?> DateTime
+                doc.Add(m.Name, dateToFlexFormat(date).ToString()) 
+            else
+                doc.Add(m.Name, (string)accessor.[instance, m.Name])
+        doc
+
+    /// Create flex document from the object using reflection
+    let createDocFromObj<'T> indexName id (instance: 'T) =
+        let doc = new Document(indexName, id)
+        
+        match typeRepository.TryGetValue(typeof<'T>.FullName) with
+        | true, t -> doc.Fields <- generateFields<'T> (instance) t
+        | _ -> 
+            typeRepository.[typeof<'T>.FullName] <- TypeAccessor.Create(typeof<'T>)
+            doc.Fields <- generateFields<'T> instance typeRepository.[typeof<'T>.FullName]
+        doc
+
+    
