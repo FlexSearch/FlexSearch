ccda6b1cd702a8b4c2c41b064c750829f1a47a91
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-08-31 22:52:03 +0100

    refactor(portal) : sessions page icons updated alongwith some minor html changes

--- srcjs/src/app/views/sessions/sessions.controller.ts
+++ srcjs/src/app/views/sessions/sessions.controller.ts
@@ -13,23 +13,46 @@ module flexportal {
     ActivePage: number
     PageCount: number
     PageSize: number
+    GridOptions: uiGrid.IGridOptions
     goToSession(sessionId: string): void
     getPage(pageNumber: number): void
     toggleRight(): void
-    closeSidenav() : void
+    closeSidenav(): void
   }
 
   export class SessionsController {
     /* @ngInject */
     constructor($scope: ISessionsScope, $state: any, $http: ng.IHttpService, datePrinter: any, flexClient: FlexClient, $mdSidenav: any, $mdUtil: any) {
-      $scope.setTitle("");
+      $scope.GridOptions = new DataGrid.GridOptions();
+      $scope.GridOptions.columnDefs = [
+        new DataGrid.ColumnDef("IndexName"),
+        new DataGrid.ColumnDef("ProfileName"),
+        new DataGrid.ColumnDef("JobStartTimeString", "Job Start Time"),
+        new DataGrid.ColumnDef("JobEndTimeString", "Job End Time"),
+        new DataGrid.ColumnDef("RecordsReturned"),
+        new DataGrid.ColumnDef("RecordsAvailable")
+      ]
+      
+      // Get data from the server
+      $scope.GridOptions.useExternalPagination = true;
+      
+      $scope.GridOptions.onRegisterApi = function(gridApi) {
+        gridApi.selection.on.rowSelectionChanged($scope, function(row) {
+          console.log(row);
+          var r = <uiGrid.IGridRow>row;
+          $state.go('session', { sessionId: r.entity.SessionId });
+        });
+        gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
+        $scope.getPage(newPage);
+      });
+      }
       
       // Handler for toggling the sidenav
       function buildToggler(navID) {
-        var debounceFn =  $mdUtil.debounce(function(){
-              $mdSidenav(navID)
-                .toggle();
-            },300);
+        var debounceFn = $mdUtil.debounce(function() {
+          $mdSidenav(navID)
+            .toggle();
+        }, 300);
         return debounceFn;
       }
       $scope.toggleRight = buildToggler('right');
@@ -41,19 +64,19 @@ module flexportal {
       
       // Helper function for navigating to a different section
       $scope.goToSession = function(sessionId) {
-        $state.go('session', {sessionId: sessionId});
+        $state.go('session', { sessionId: sessionId });
       };
       
       // Function for monitoring the sidenav close
       $scope.$watch(
-        function () { return $mdSidenav('right').isOpen(); },
-        function (newValue, oldValue) {
+        function() { return $mdSidenav('right').isOpen(); },
+        function(newValue, oldValue) {
           if (newValue == false)
             $state.go('sessions');
         });
       
       // Initialize paging
-      $scope.PageSize = 20;
+      $scope.PageSize = 50;
       $scope.ActivePage = 1;
       $scope.getPage = function(pageNumber) {
         // Display progress bar
@@ -66,22 +89,23 @@ module flexportal {
         
         // Get the sessions
         flexClient.getSessions(
-          $scope.PageSize, 
+          $scope.PageSize,
           ($scope.ActivePage - 1) * $scope.PageSize,
-          "timestamp", "desc" )
-        .then(results => {
-          $scope.Sessions = results.Documents
-            .map(d => <Session>JSON.parse(d.Fields["sessionproperties"]))
-            .map(s => {
-              s.JobStartTimeString = datePrinter.toDateStr(s.JobStartTime);
-              s.JobEndTimeString = datePrinter.toDateStr(s.JobEndTime);
-              return s;
-            });
-            
-          // Set the number of pages
-          $scope.PageCount = Math.ceil(results.TotalAvailable / $scope.PageSize);
-        })
-        .then(() => progress.hide());
+          "timestamp", "desc")
+          .then(results => {
+            $scope.Sessions = results.Documents
+              .map(d => <Session>JSON.parse(d.Fields["sessionproperties"]))
+              .map(s => {
+                s.JobStartTimeString = datePrinter.toDateStr(s.JobStartTime);
+                s.JobEndTimeString = datePrinter.toDateStr(s.JobEndTime);
+                return s;
+              });
+
+            $scope.GridOptions.data = $scope.Sessions;
+            // Set the number of pages
+            $scope.PageCount = Math.ceil(results.TotalAvailable / $scope.PageSize);
+          })
+          .then(() => progress.hide());
       };
       
       // Get the active page
