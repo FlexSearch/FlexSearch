efa4fcc79532c9c4e07262032735b33f6ccc70cb
vladnega <negacevschi.vladimir@gmail.com>
2016-01-08 13:59:53 +0000

    fix(api) : don't ignore codegen-config.json

--- scripts/GenerateAPI.fsx
+++ scripts/GenerateAPI.fsx
@@ -9,8 +9,9 @@ open System.Text.RegularExpressions
 
 let private generateSwaggerModel() = 
     !>> "Cleaning Models, Api & Client directories"
+    [modelsTempDir; modelsDir; apiDir; clientDir] |> Seq.iter (ensureDir >> ignore)
     [modelsTempDir; modelsDir; apiDir; clientDir] |> Seq.iter emptyDir
-    javaExec <| sprintf """-jar %s generate -i %s -l csharp -t %s -o obj -c %s""" (toolsDir <!!> "swagger-codegen-cli.jar") (specDir <!!> "swagger-partial.json") (specDir <!!> "template") (toolsDir <!!> "codegen-config.json")
+    javaExec <| sprintf """-jar %s generate -i %s -l csharp -t %s -o obj -c %s""" (toolsDir <!!> "swagger-codegen-cli.jar") (specDir <!!> "swagger-partial.json") (specDir <!!> "template") (toolsDir <!!> @"..\codegen-config.json")
     let generatedFiles = Directory.GetFiles(modelsTempDir).Count()
     if generatedFiles > 0 then
         brk()
