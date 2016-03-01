b5a6faf5a96de592fc697c0dfaac83be52e22c32
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2016-03-01 10:57:00 +0000

    feat(index) : use pre index script before using the Lucene document

--- src/FlexSearch.Core/Index/DocumentTemplate.fs
+++ src/FlexSearch.Core/Index/DocumentTemplate.fs
@@ -59,8 +59,12 @@ module DocumentTemplate =
     
     /// Update the lucene Document based upon the passed FlexDocument.
     /// Note: Do not update the document from multiple threads.
-    let updateTempate (document : Document) (modifyIndex : int64) (template : DocumentTemplate) = 
-        document.ModifyIndex <- modifyIndex
+    let updateTempate (document : Document) (scripts : Scripts) (template : DocumentTemplate) = 
+        try 
+            match scripts.PreIndexScript with
+            | Some(s) -> s.Invoke(document)
+            | _ -> ()
+        with e -> Logger.Log("Script execution error.", e, MessageKeyword.Document, MessageLevel.Error)
         for i = 0 to template.Setting.Fields.Count - 1 do
             let f = template.Setting.Fields.[i]
             let tf = template.TemplateFields.[i]
